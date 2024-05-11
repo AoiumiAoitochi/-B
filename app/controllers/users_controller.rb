678c9e1b189e6e38e58447bfe193461a4adf6851
Aoi <hoyagaoishii@gmail.com>
2024-05-11 20:28:33 +0900

    8.1バリデーション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,13 +11,28 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user # 保存成功後、ログインします。
+      log_in @user
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
 
   private
 
