9b721ee2310562aef7562298c58a5038ed19797a
Aoi <hoyagaoishii@gmail.com>
2024-05-14 14:08:58 +0900

    Finish add-basic-info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  
 
   def index
     @users = User.paginate(page: params[:page])
@@ -22,7 +23,7 @@ class UsersController < ApplicationController
       flash[:success] = '新規作成に成功しました。'
       redirect_to @user
     else
-     render 'new', status: :unprocessable_entity
+      render :new
     end
   end
 
@@ -34,7 +35,7 @@ class UsersController < ApplicationController
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
-      render 'edit', status: :unprocessable_entity
+      render :edit
     end
   end
 
@@ -44,6 +45,30 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def edit_basic_info
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html { render partial: 'users/edit_basic_info', locals: { user: @user } }
+      format.turbo_stream
+    end
+  end
+
+  def update_basic_info
+    if @user.update(basic_info_params)
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.turbo_stream
+    end
+  end
+
+
+
   private
 
     def user_params
@@ -51,6 +76,9 @@ class UsersController < ApplicationController
     end
 
     # beforeフィルター
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
+    end
 
     # paramsハッシュからユーザーを取得します。
     def set_user
