afde18fc790cfa03b42e17e051352a3df89b4811
Aoi <hoyagaoishii@gmail.com>
2024-05-21 13:23:26 +0900

    勤怠編集機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,11 @@ class UsersController < ApplicationController
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
   before_action :set_one_month, only: :show
 
+
   def index
     @users = User.paginate(page: params[:page])
   end
+
 
   def show
     @worked_sum = @attendances.where.not(started_at: nil).count
@@ -17,6 +19,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
 
   def create
     @user = User.new(user_params)
@@ -29,8 +32,10 @@ class UsersController < ApplicationController
     end
   end
 
+
   def edit
   end
+
 
   def update
     if @user.update(user_params)
@@ -41,20 +46,25 @@ class UsersController < ApplicationController
     end
   end
 
+
   def destroy
     @user.destroy
     flash[:success] = "#{@user.name}のデータを削除しました。"
     redirect_to users_url
   end
 
+
   def edit_basic_info
     @user = User.find(params[:id])
 
     respond_to do |format|
-      format.html { render partial: 'users/edit_basic_info', locals: { user: @user } }
+      format.html { render partial: 'users/edit_basic_info', locals: { user: @user } } # 修正
       format.turbo_stream
     end
   end
+
+
+
 
   def update_basic_info
     if @user.update(basic_info_params)
@@ -69,40 +79,9 @@ class UsersController < ApplicationController
     end
   end
 
-
-
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
-    end
-
-    # beforeフィルター
-    def basic_info_params
-      params.require(:user).permit(:department, :basic_time, :work_time)
-    end
-
-    # paramsハッシュからユーザーを取得します。
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    # ログイン済みのユーザーか確認します。
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
-
-    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
-    def correct_user
-      redirect_to(root_url) unless current_user?(@user)
-    end
-
-    # システム管理権限所有かどうか判定します。
-    def admin_user
-      redirect_to root_url unless current_user.admin?
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+  end
 end
